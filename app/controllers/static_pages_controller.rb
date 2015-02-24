7db46b6421344cf361e1b4770fb2c489c4f15c1b
Kamil Tarnowski <kam.tarnowski@gmail.com>
2015-02-24 20:10:28 +0100

    different results implemented

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,21 @@
 class StaticPagesController < ApplicationController
+
   def table
-    @books = Book.page params[:page]
+
+    @per_page = params[:per_page] || 10
+    @books = Book.page(params[:page]).per(@per_page)
     respond_to do |format|
-      format.html # index.html.erb
+      format.html
       ajax_respond format, :section_id => "page"
     end
   end
 
   def mobile
+  end
+
+  protected
+
+  def set_content_type
+    params[:content_type] ||= "/?per_page=20"
   end
 end
